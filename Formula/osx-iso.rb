e7e6818f3f4c50cda8950984dc94be44a2cbd216
Buster Collings <busterc@gmail.com>
2019-10-10 11:47:29 -0500

    Update for v5
    - new url and sha
    - osx-iso now supports catalina

--- Formula/osx-iso.rb
+++ Formula/osx-iso.rb
@@ -1,8 +1,8 @@
 class OsxIso < Formula
   desc "Create a bootable ISO of OS X / macOS, from the installation app file"
   homepage "https://github.com/busterc/osx-iso"
-  url "https://github.com/busterc/osx-iso/archive/v4.0.0.tar.gz"
-  sha256 "ad8d5012f07fad77e58c37216992ebd252bdd8af793f03a24a93c80bfbd27b02"
+  url "https://github.com/busterc/osx-iso/archive/v5.0.0.tar.gz"
+  sha256 "36435edf98e92a1c326c219263fe004e7fce121bcac24ab8e802f4dd200807cd"
   head "https://github.com/busterc/osx-iso.git"
 
   def install
