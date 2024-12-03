fadcf49bdb301d3d0ee34e2200c2cadf68f0a5f5
startergo <7897244+startergo@users.noreply.github.com>
2024-12-03 13:13:11 +0300

    Update osx-iso.rb

--- Formula/osx-iso.rb
+++ Formula/osx-iso.rb
@@ -1,7 +1,7 @@
 class OsxIso < Formula
   desc "Create a bootable ISO of OS X / macOS, from the installation app file"
   homepage "https://github.com/startergo/osx-iso"
-  url "https://github.com/startergo/osx-iso/archive/refs/tags/v6.0.3.tar.gz"
+  url "https://github.com/startergo/osx-iso/archive/refs/tags/v6.0.4.tar.gz"
   sha256 "ddb87e033645d45cf4015e1d9ee548e9963c8c302918c904ba5a222a44efb005"
   head "https://github.com/startergo/osx-iso.git"
 
